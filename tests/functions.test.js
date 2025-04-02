4a5c4f57df361f13c4680cf2282897f596d20785
sami-chanane <sami.chanane.99@gmail.com>
2025-04-02 09:01:15 +0200

    final test 2

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -22,25 +22,25 @@ describe('subtract', ()=>{
     })
 })
 
-// describe('multiply', ()=>{
-//     it('should return the multiplication of 2 numbers', ()=>{
-//         expect(multiply(2,2)).toBe(4);
-//         expect(multiply(17,-3)).toBe(-51);
-//         expect(multiply(0,7)).toBe(0);
-//         expect(multiply(10,100)).toBe(1000);
-//         expect(multiply(32,-22)).toBe(-704);
-//     })
-// })
+describe('multiply', ()=>{
+    it('should return the multiplication of 2 numbers', ()=>{
+        expect(multiply(2,2)).toBe(4);
+        expect(multiply(17,-3)).toBe(-51);
+        expect(multiply(0,7)).toBe(0);
+        expect(multiply(10,100)).toBe(1000);
+        expect(multiply(32,-22)).toBe(-704);
+    })
+})
 
 
-// describe('divide', ()=>{
-//     it('should return the division of 2 numbers', ()=>{
-//         expect(divide(2,2)).toBe(1);
-//         expect(divide(50,-5)).toBe(-10);
-//         expect(divide(0,7)).toBe(0);
-//         expect(divide(32,0)).toBe("Cannot divide by zero");
-//         expect(divide(-18,-18)).toBe(1);
-//     })
-// })
+describe('divide', ()=>{
+    it('should return the division of 2 numbers', ()=>{
+        expect(divide(2,2)).toBe(1);
+        expect(divide(50,-5)).toBe(-10);
+        expect(divide(0,7)).toBe(0);
+        expect(divide(32,0)).toBe("Cannot divide by zero");
+        expect(divide(-18,-18)).toBe(1);
+    })
+})
 
 
